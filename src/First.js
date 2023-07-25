d33cbaad36477eb7f9be834d1d9efc16e81b191c
girinath <girinathpatil@gmail.com>
2023-07-25 19:13:31 +0530

    cc

---
+++ src/First.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Second from "./Second";
+
+function First(){
+
+    return(
+        <React.Fragment>
+            <Second key1={"Hello_01"}
+                    key2={100}
+                    key3={true}></Second>
+        </React.Fragment>
+    )
+}
+
+export default First;
